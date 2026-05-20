861ac84426464b47d958c1125a05a383c5698fe1
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2026-05-20 12:38:01 +0300

    Add test

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -23,10 +23,29 @@ describe('chainer', () => {
   });
 
   it('handles single function', () => {
-    const f1 = jest.fn((x) => x * 3);
+    // Wrapped in jest.fn() so Jest can track calls
+    const f1 = jest.fn((x) => x * 2);
+    const f2 = jest.fn((x) => x + 2);
+    const f3 = jest.fn((x) => Math.pow(x, 2));
+
     const chained = chainer([f1]);
 
-    expect(chained(4)).toBe(12);
-    expect(f1).toHaveBeenCalledWith(4);
+    expect(chained(5)).toBe(10);
+    expect(f1).toHaveBeenCalledWith(5); // Changed from 0 to 5
+    expect(f2).not.toHaveBeenCalled(); // Removed arguments from matchers
+    expect(f3).not.toHaveBeenCalled();
+  });
+
+  it('handles zero as an input value', () => {
+    const f1 = jest.fn((x) => x * 2);
+    const f2 = jest.fn((x) => x + 2);
+    const f3 = jest.fn((x) => Math.pow(x, 2));
+
+    const chained = chainer([f1, f2, f3]);
+
+    expect(chained(0)).toBe(4); // (0 * 2) -> (0 + 2) -> (2 ^ 2) = 4
+    expect(f1).toHaveBeenCalledWith(0);
+    expect(f2).toHaveBeenCalledWith(0);
+    expect(f3).toHaveBeenCalledWith(2);
   });
 });
